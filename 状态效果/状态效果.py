5bf98014d1f4b58e5eb47dea7f30c2b9c8e45392
AlbertZhang <itsalbertzhang@gmail.com>
2022-07-13 00:46:30 +0800

    2207130046

--- 状态效果/状态效果.py
+++ 状态效果/状态效果.py
@@ -5,7 +5,7 @@ from 数据输入 import 玩家属性
 
 
 class 状态效果():
-    def __init__(self, 作用属性, 作用数值: 0, 作用1024分值: 0, 作用对象: None, 作用于心法主属性=False) -> None:
+    def __init__(self, 作用属性, 作用数值=0, 作用1024分值=0, 作用对象=None, 作用于心法主属性=False) -> None:
         '''
         如果该状态效果作用于心法主属性, 那么需要使用 '定义作用对象' 方法传入一个个体对象, 以便调用 '个体对象' 类中的心法主属性变动方法.
         '''
@@ -29,20 +29,20 @@ class 状态效果():
     def 获得效果(self):
         for elem in self.作用属性:
             if not self.作用于心法主属性:
-                self.作用属性[elem].额外 = self.作用数值
-                self.作用属性[elem].额外1024分比 = self.作用1024分值
+                elem.额外 = self.作用数值
+                elem.额外1024分比 = self.作用1024分值
             else:
                 try:
-                    if self.作用对象.体质 == self.作用属性[elem]:
-                        self.作用对象.体质变动(self.作用数值, self.作用1024分值)
-                    if self.作用对象.元气 == self.作用属性[elem]:
-                        self.作用对象.元气变动(self.作用数值, self.作用1024分值)
-                    if self.作用对象.根骨 == self.作用属性[elem]:
-                        self.作用对象.根骨变动(self.作用数值, self.作用1024分值)
-                    if self.作用对象.力道 == self.作用属性[elem]:
-                        self.作用对象.力道变动(self.作用数值, self.作用1024分值)
-                    if self.作用对象.身法 == self.作用属性[elem]:
-                        self.作用对象.身法变动(self.作用数值, self.作用1024分值)
+                    if self.作用对象.属性集.体质 == elem:
+                        self.作用对象.体质变更(self.作用数值, self.作用1024分值)
+                    if self.作用对象.属性集.元气 == elem:
+                        self.作用对象.元气变更(self.作用数值, self.作用1024分值)
+                    if self.作用对象.属性集.根骨 == elem:
+                        self.作用对象.根骨变更(self.作用数值, self.作用1024分值)
+                    if self.作用对象.属性集.力道 == elem:
+                        self.作用对象.力道变更(self.作用数值, self.作用1024分值)
+                    if self.作用对象.属性集.身法 == elem:
+                        self.作用对象.身法变更(self.作用数值, self.作用1024分值)
                 except:
                     print('未能成功获得效果.')
 
@@ -52,3 +52,23 @@ class 状态效果():
         self.获得效果()
         self.作用数值 = -self.作用数值
         self.作用1024分值 = -self.作用1024分值
+
+
+class 状态集():
+    def __init__(self, 状态dict={}) -> None:
+        self.批量初始化并获得效果(状态dict)
+
+    def 批量初始化并获得效果(self, 状态dict: dict) -> None:
+        '''
+        初始化单个效果可以直接用 obj.attr 的方式
+        '''
+        for key in 状态dict:
+            setattr(self, key, 状态dict[key])
+            getattr(self, key).获得效果()
+
+    def 批量失去效果(self, 状态dict: dict) -> None:
+        '''
+        失去单个效果可以直接用 obj.attr.失去效果() 的方式
+        '''
+        for key in 状态dict:
+            getattr(self, key).失去效果()
